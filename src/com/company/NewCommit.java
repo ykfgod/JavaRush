b06d34e3f42455a48e6d104daaffcbe896903467
ykf <ykfgodnew@gmail.com>
2022-02-11 11:03:49 +0300

    Init

---
+++ src/com/company/NewCommit.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class NewCommit {
+    public static void main (String[] args) {
+        System.out.println("Новый комит");
+        System.out.println("Еще один комит");
+    }
+}
