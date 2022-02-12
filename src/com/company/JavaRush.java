f94b34c38a53a2a8553de7f5ee9c1b9a8fd11fa0
ykf <ykfgodnew@gmail.com>
2022-02-12 16:30:43 +0300

    Треугольник

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -9,30 +9,23 @@ import java.util.Scanner;
 public class JavaRush {
     public static void main (String [] args) {
 
-        int a, b, c;
-        boolean isHigh = false, isLow = false;
+        // Треугольник
 
         Scanner console = new Scanner(System.in);
+        int a, b, c;
 
-        System.out.println("Введите 1 число");
+        System.out.println("Введите первую сторону треугольника");
         a = console.nextInt();
 
-        System.out.println("Введите 2число");
+        System.out.println("Введите вторую сторону треугольника");
         b = console.nextInt();
 
-        System.out.println("Введите 3число");
+        System.out.println("Введите третью сторону треугольника");
         c = console.nextInt();
 
-        if (a == b) {
-            if (a == c)
-            System.out.println(a + " " + b + " " + c);
-            else System.out.println(a + " "  + b);
-        }
-        else if (b == c)
-            System.out.println(b + " " + c);
-        else if (a == c)
-            System.out.println(a + " " + c);
+        if ((a + b > c) && (a + c > b) && (b + c > a))
+            System.out.println("Можно составить треугольник");
         else
-            System.out.println("Нет равных чисел");
+            System.out.println("Нельзя составить треугольник");
     }
 }
