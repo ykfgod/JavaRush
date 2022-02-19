090d6b0dfb2a27ba4bc7c4204f3c2171acfb08b0
ykf <ykfgodnew@gmail.com>
2022-02-19 15:05:21 +0300

    JavaRush 4 уровень,  6 лекция Сумма кратных чисел

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -3,14 +3,32 @@ package com.company;
 import java.util.Scanner;
 
 /*
-    JavaRush 4 уровень,  6 лекция Четные числа
+    JavaRush 4 уровень,  6 лекция Сумма кратных чисел
  */
 public class JavaRush {
 
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int start, end, multiple;
+        int count = 0;
 
-        for (int i = 1; i<= 15; i++)
-            if (i % 2 == 0)
-                System.out.println(i);
+        System.out.println("Введите начальное число");
+        start = console.nextInt();
+        System.out.println("Введите последнее число");
+        end = console.nextInt();
+        System.out.println("Введите кратное число");
+        multiple = console.nextInt();
+
+
+        for (;start < end;) {
+            if (start % multiple == 0) {
+                System.out.println(start);
+                count += start;
+            }
+
+            start++;
+            continue;
+        }
+        System.out.println(count);
     }
 }
