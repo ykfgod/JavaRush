275a877f346d89e97ea24970935c64d083da64ed
ykf <ykfgodnew@gmail.com>
2022-02-19 09:11:12 +0300

    JavaRush 4 уровень,  4 лекция Сумма чисел, не кратных 3

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -3,33 +3,23 @@ package com.company;
 import java.util.Scanner;
 
 /*
-    JavaRush 4.3 Незаполненный прямоугольник
+    JavaRush 4 уровень,  4 лекция Сумма чисел, не кратных 3
  */
 public class JavaRush {
 
     public static void main(String[] args) {
-        int n = 0;
+        Scanner console = new Scanner(System.in);
+        int i = 0;
+        int sum = 0;
 
-        while (n < 10) {
-
-            int m = 0 ;
-
-            if (n == 0 || n == 9) {
-                while (m < 20) {
-                    System.out.print('Б');
-                    m++;
-                }
-            } else {
-                System.out.print("Б");
-                while (m < 18) {
-                    System.out.print(' ');
-                    m++;
-                }
-                System.out.print("Б");
-            }
-
-            n++;
-            System.out.println();
+        while (i < 100) {
+            i++;
+            if ((i % 3) == 0)
+             continue;
+            System.out.println(i);
+            sum += i;
         }
+
+        System.out.println(sum);
     }
 }
