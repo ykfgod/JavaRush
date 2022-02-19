1ca9c9c11469f1e85ab2d3152d126251882a7e65
ykf <ykfgodnew@gmail.com>
2022-02-19 14:10:27 +0300

    JavaRush 4 уровень,  6 лекция Четные числа

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -3,31 +3,14 @@ package com.company;
 import java.util.Scanner;
 
 /*
-    JavaRush 4 уровень,  5 лекция Второе минимальное число из введенных
+    JavaRush 4 уровень,  6 лекция Четные числа
  */
 public class JavaRush {
 
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
 
-        int i = 0;
-        int min = Integer.MAX_VALUE;
-        int min2 = Integer.MAX_VALUE;
-
-        while (console.hasNextInt()) {
-            i = console.nextInt();
-
-            if (i < min) {
-                min2 = min;
-                min = i;
-            }
-
-            if ((i < min2) && (min != i))
-                min2 = i;
-        }
-
-        System.out.println(min);
-        System.out.println(min2);
-
+        for (int i = 1; i<= 15; i++)
+            if (i % 2 == 0)
+                System.out.println(i);
     }
 }
