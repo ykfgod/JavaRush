a5b0895d5fd4a61dac218198023b0305e077cce8
ykf <ykfgodnew@gmail.com>
2022-02-19 07:34:50 +0300

    JavaRush 4.3 Незаполненный прямоугольник

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -3,20 +3,29 @@ package com.company;
 import java.util.Scanner;
 
 /*
-    JavaRush 4.3 Заполненный прямоугольник
+    JavaRush 4.3 Незаполненный прямоугольник
  */
 public class JavaRush {
 
     public static void main(String[] args) {
         int n = 0;
 
-        while (n < 5) {
+        while (n < 10) {
 
             int m = 0 ;
 
-            while (m < 10){
-                System.out.print('Q');
-                m++;
+            if (n == 0 || n == 9) {
+                while (m < 20) {
+                    System.out.print('Б');
+                    m++;
+                }
+            } else {
+                System.out.print("Б");
+                while (m < 18) {
+                    System.out.print(' ');
+                    m++;
+                }
+                System.out.print("Б");
             }
 
             n++;
