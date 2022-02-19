588b652bf8b592f4df60ae9e9a10543c3d98981d
ykf <ykfgodnew@gmail.com>
2022-02-19 07:02:44 +0300

    JavaRush 4.3 Все любят Мамбу

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -3,20 +3,21 @@ package com.company;
 import java.util.Scanner;
 
 /*
-    JavaRush 4.3 Я никогда не буду работать за копейки
+    JavaRush 4.3 Все любят Мамбу
  */
 public class JavaRush {
 
     public static void main(String[] args) {
 
+        Scanner console = new Scanner(System.in);
         int n = 0;
-        int count = 0;
-        while (n < 100) {
-            System.out.println("Я никогда не буду работать за копейки. Амиго");
-            n++;
-            count++;
-        }
+        String name;
 
-        System.out.println(count);
+        name = console.nextLine();
+
+        while (n < 10) {
+            System.out.println(name + " любит меня");
+            n++;
+        }
     }
 }
