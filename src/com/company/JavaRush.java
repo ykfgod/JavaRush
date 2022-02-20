dceb0ce3bbe417bba12747f75c91b16b056c6ca4
ykf <ykfgodnew@gmail.com>
2022-02-20 11:00:03 +0300

    JavaRush 5 уровень,  8 лекция Выполнение фрагмента кода

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -3,16 +3,13 @@ package com.company;
 import java.util.Scanner;
 
 /*
-    JavaRush 5 уровень,  8 лекция Переобуваемся на лету
+    JavaRush 5 уровень,  8 лекция Выполнение фрагмента кода
  */
 public class JavaRush {
-    public static int result = 105;
+    public static int result = 45967;
 
     public static void main(String[] args) {
-        byte correction = Byte.MAX_VALUE;
-        for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {
-            correction *= fahrenheit;
-            System.out.println(correction);
-        }
+        String loop = "    for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {\n";
+        System.out.println(loop);
     }
 }
