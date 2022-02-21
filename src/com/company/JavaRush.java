d4b83e8cd9e56091f8c4854deb010d7a2f3dc401
ykf <ykfgodnew@gmail.com>
2022-02-21 08:24:48 +0300

    JavaRush 6 уровень,  1 лекция Выполнение фрагмента кода

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -6,9 +6,17 @@ import java.util.Scanner;
     JavaRush 6 уровень,  1 лекция Выполнение фрагмента кода
  */
 public class JavaRush {
+    public static final String ODD = "Нечётный";
+    public static final String EVEN = "Чётный";
+    public static String[] strings = new String[20];
 
     public static void main(String[] args) {
-       int[] intArray = new int[10];
-       double[] doubleArray = new double[10];
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0)
+                strings[i] = EVEN;
+            else
+                strings[i] = ODD;
+            System.out.println(strings[i]);
+        }
     }
 }
