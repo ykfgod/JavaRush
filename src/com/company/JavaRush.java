447eb2778e6fd2debac58e4a2b913aeafbca3292
ykf <ykfgodnew@gmail.com>
2022-02-21 11:04:21 +0300

    JavaRush  6 уровень, 3 лекция Revers

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -3,34 +3,32 @@ package com.company;
 import java.util.Scanner;
 
 /*
-    JavaRush  6 уровень, 2 лекция Объединяем массивы
+    JavaRush  6 уровень, 3 лекция Revers
  */
 public class JavaRush {
 
-    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-    public static int[] resultArray;
-
     public static void main(String[] args) {
 
-        resultArray = new int[firstArray.length + secondArray.length];
+        Scanner console = new Scanner(System.in);
+        int N;
+        int [] array;
 
-        for (int i = 0; i < firstArray.length; i++) {
-            resultArray[i] = firstArray[i];
+        System.out.println("Введите число элементов");
+        N = console.nextInt();
+        array = new int[N];
+
+        System.out.println("Введите " + N + " чисел");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = console.nextInt();
         }
 
-        int j = 0;
-
-        for (int i = firstArray.length; i < resultArray.length; i++) {
-
-            resultArray[i] = secondArray[j];
-            j++;
-        }
-
-        for (int i = 0; i < resultArray.length; i++) {
-            System.out.println(resultArray[i]);
-        }
-
+        if (N % 2 == 0) {
+            for (int i = array.length-1; i >= 0; i--)
+                System.out.println(array[i]);
+        } else
+            for (int i = 0; i < array.length; i++) {
+                System.out.println(array[i]);
+            }
     }
 
 }
