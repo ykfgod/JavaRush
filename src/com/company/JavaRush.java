f8236695c297e224a379003899e7f87c652d482f
ykf <ykfgodnew@gmail.com>
2022-02-21 08:29:31 +0300

    JavaRush 6 уровень,  1 лекция Работаем с элементами массива

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -6,17 +6,15 @@ import java.util.Scanner;
     JavaRush 6 уровень,  1 лекция Выполнение фрагмента кода
  */
 public class JavaRush {
-    public static final String ODD = "Нечётный";
-    public static final String EVEN = "Чётный";
-    public static String[] strings = new String[20];
+
+    public static int[] array = new int[]{-1, 2, 3, -4, -5};
 
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 5; i++) {
             if (i % 2 == 0)
-                strings[i] = EVEN;
-            else
-                strings[i] = ODD;
-            System.out.println(strings[i]);
+                array[i] = array[i] * -1;
+
+        System.out.println(array[i]);
         }
     }
 }
