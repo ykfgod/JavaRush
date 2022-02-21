f5582094182ad7fc6f09c00a73e12a6ab6fc8047
ykf <ykfgodnew@gmail.com>
2022-02-21 10:31:16 +0300

    JavaRush  6 уровень, 2 лекция Объединяем массивы

--- src/com/company/JavaRush.java
+++ src/com/company/JavaRush.java
@@ -3,18 +3,34 @@ package com.company;
 import java.util.Scanner;
 
 /*
-    JavaRush 6 уровень,  1 лекция Выполнение фрагмента кода
+    JavaRush  6 уровень, 2 лекция Объединяем массивы
  */
 public class JavaRush {
 
-    public static int[] array = new int[]{-1, 2, 3, -4, -5};
+    public static int[] firstArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static int[] secondArray = new int[]{10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+    public static int[] resultArray;
 
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            if (i % 2 == 0)
-                array[i] = array[i] * -1;
 
-        System.out.println(array[i]);
+        resultArray = new int[firstArray.length + secondArray.length];
+
+        for (int i = 0; i < firstArray.length; i++) {
+            resultArray[i] = firstArray[i];
         }
+
+        int j = 0;
+
+        for (int i = firstArray.length; i < resultArray.length; i++) {
+
+            resultArray[i] = secondArray[j];
+            j++;
+        }
+
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.println(resultArray[i]);
+        }
+
     }
+
 }
